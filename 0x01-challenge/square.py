b49f62efd03a78b7189d62476b89cec1028b21e6
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-05-15 22:13:49 +0100

    Task 1 v0.6

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -30,7 +30,7 @@ class Square():
 
 if __name__ == "__main__":
     """ Main function to execut the square class """
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
