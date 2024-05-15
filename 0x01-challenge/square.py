660d9759acac8c9c5b77f9730c9aadab5de4e785
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-05-15 22:06:43 +0100

    Task1 fix pycodestyle

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Code defining square class
+and its methods for calculating area and perimeter.
+"""
+
 
 class square():
     """My square class."""
@@ -6,6 +11,7 @@ class square():
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """Init the square class."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,13 +20,16 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Permiter of the Square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Resturns a string representation of the suare."""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
+    """ Main function to execut the square class """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
